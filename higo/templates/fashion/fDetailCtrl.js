8c0b243a4e69a6cb5becef77c9b74cfc2fefa696
wangjing <554761618@qq.com>
2017-04-04 18:48:33 +0800

    delete

--- higo/templates/fashion/fDetailCtrl.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Useradmin on 2016/11/27.
- */
-angular.module("myapp").controller("fDetailCtrl", function ($scope,$http,$state,$stateParams) {
-    $scope.selectFashion=JSON.parse($stateParams.fashionInfo)
-})
--- 北苑大酒店/js/callOrder.js
+++
@@ -1,9 +0,0 @@
-
-$(function(){
-    $(".section1").find(".order").on("touchstart",function(){
-        $(".section1").find(".phone").css("display","block")
-    });
-    $(".section1").find(".cancel").on("touchstart",function(){
-        $(".section1").find(".phone").css("display","none")
-    })
-})
